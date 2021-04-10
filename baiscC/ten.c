6b55380e363acb92c463d2cf17263c7a2851980e
urvashi2213 <urvashi2213@gmail.com>
2021-04-10 17:04:09 +0530

    tenth program of c

---
+++ baiscC/ten.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    float fh;
+    printf("Enter Fahreinheit=");
+    scanf("%f",&fh);
+    printf("\n Conversion of fh into c=%f",(fh-32)*5/9);
+    return 0;
+
+    return 0;
+}
+
